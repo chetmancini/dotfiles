d6ddf5a4ae5b931d43a32f45b7d386fbf8d07380
Chet Mancini <chetmancini@gmail.com>
2025-01-02 21:06:00 -0500

    updates

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,23 @@
 return {
-  "tokyonight.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    return {
-      transparent = true,
-    }
-  end,
+    "catppuccin/nvim",
+    -- "sainnhe/sonokai",
+    -- "folke/tokyonight.nvim",
+    -- "rose-pine/neovim",
+    -- name = "sonokai",
+    -- name = "rose-pine",
+    -- name = "tokyonight",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        --vim.cmd("colorscheme rose-pine"),
+        -- vim.cmd.colorscheme("rose-pine")
+        -- vim.cmd.colorscheme("tokyonight")
+        vim.cmd.colorscheme("catppuccin")
+        -- vim.cmd.colorscheme("sonokai")
+    end,
+    opts = function()
+        return {
+            transparent = true,
+        }
+    end,
 }
